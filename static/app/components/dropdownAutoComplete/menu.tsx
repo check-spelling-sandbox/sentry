2277a34de8f8fa9c8d3207b99b26bb736252e5a3
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-21 21:35:18 -0400

    spelling: selectable
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- static/app/components/dropdownAutoComplete/menu.tsx
+++ static/app/components/dropdownAutoComplete/menu.tsx
@@ -262,7 +262,7 @@ const Menu = ({
       const showInput = !hideInput && (hasItems || !emptyHidesInput);
 
       // When virtualization is turned on, we need to pass in the number of
-      // selecteable items for arrow-key limits
+      // selectable items for arrow-key limits
       const itemCount = virtualizedHeight
         ? autoCompleteResults.filter(i => !i.groupLabel).length
         : undefined;
