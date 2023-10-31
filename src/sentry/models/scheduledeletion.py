5ed993212a1c312e78ed8be44e9f4dbc1edeb58b
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-10-30 22:38:43 -0400

    spelling: preempt
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/sentry/models/scheduledeletion.py
+++ src/sentry/models/scheduledeletion.py
@@ -64,7 +64,7 @@ class BaseScheduledDeletion(Model):
         model = type(instance)
         silo_mode = SiloMode.get_current_mode()
         if silo_mode not in model._meta.silo_limit.modes and silo_mode != SiloMode.MONOLITH:
-            # Pre-empt the fact that our silo protections wouldn't fire for mismatched model <-> silo deletion objects.
+            # Preempt the fact that our silo protections wouldn't fire for mismatched model <-> silo deletion objects.
             raise model._meta.silo_limit.AvailabilityError(
                 f"{model!r} was scheduled for deletion by {cls!r}, but is unavailable in {silo_mode!r}"
             )
